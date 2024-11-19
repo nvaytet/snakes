4bdf220e7a6941f75303162a1869bc87b5e4454c
Neil Vaytet <neil.vaytet@ess.eu>
2024-11-19 22:58:06 +0100

    improve fps

--- src/snakes/config.py
+++ src/snakes/config.py
@@ -15,10 +15,9 @@ class Config:
         self.ny: int = self.window_size[1] // self.scaling
         self.thickness = 3
         self.powerup_size = 32
-        # self.powerup_duration = 8 * self.fps
         self.gap_period = 8.0
         self.gap_duration = 0.25
         self.max_powerups = 2
-        self.no_powerups = 1  # 0
+        self.no_powerups = 10
         self.speed: float = 60.0
         self.high_score = 100
